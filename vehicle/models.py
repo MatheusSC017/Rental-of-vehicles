f72784e37fe7d08cb5bd057f573370686b020c1d
Matheus Simão Caixeta <101995486+MatheusSC017@users.noreply.github.com>
2022-08-18 20:12:33 -0300

    Criação da classe modelo de veiculo

---
+++ vehicle/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+
+class Vehicle(models.Model):
+    FUEL = (
+        ('G', 'Gasolina'),
+        ('E', 'Etanol'),
+        ('D', 'Diesel'),
+        ('H', 'Hibrido'),
+    )
+
+    TYPE = (
+        ('M', 'Moto'),
+        ('C', 'Carro'),
+    )
+
+    type_vehicle = models.CharField(max_length=1, default='M', choices=TYPE, verbose_name='veículo')
+    brand_vehicle = models.CharField(max_length=100, verbose_name='marca')
+    model_vehicle = models.CharField(max_length=100, verbose_name='modelo')
+    year_manufacure_vehicle = models.CharField(max_length=4, verbose_name='ano de fabricação')
+    model_year_vehicle = models.CharField(max_length=4, verbose_name='ano do modelo')
+    mileage_vehicle = models.FloatField(default=0, verbose_name='quilometragem')
+    license_plate_vehicle = models.CharField(max_length=7, unique=True, verbose_name='placa')
+    chassi_vehicle = models.CharField(max_length=17, unique=True, verbose_name='chassi')
+    fuel_vehicle = models.CharField(max_length=1, default='G', choices=FUEL, verbose_name='combustível')
+    fuel_tank_vehicle = models.PositiveSmallIntegerField(verbose_name='capacidade do tanque')
+    engine_vehicle = models.CharField(max_length=100, verbose_name='motor')
+    color_vehicle = models.CharField(max_length=25, verbose_name='cor')
+    other_data_vehicle = models.CharField(max_length=500, blank=True, null=True, verbose_name='mais informações')
+
+    class Meta:
+        verbose_name = 'veículo'
