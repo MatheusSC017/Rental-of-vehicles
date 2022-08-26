f7f47c74c130b4a6de7886f4bfd49389a416ce2f
Matheus Simão Caixeta <101995486+MatheusSC017@users.noreply.github.com>
2022-08-26 18:30:21 -0300

    Criação da função para geração de filiais

--- fake_data_generator.py
+++ fake_data_generator.py
@@ -8,6 +8,7 @@ import faker
 from random import randrange, choice
 from address.models import Address
 from client.models import Client
+from branch.models import Branch
 from django.contrib.auth.models import User
 
 fake = faker.Faker('pt_BR')
@@ -78,6 +79,28 @@ def client_generator():
     return client
 
 
+def branch_generator():
+    name_branch = fake.street_name()
+    start = randrange(5, 13)
+    opening_hours_start = str(start) + ':00:00'
+    opening_hours_end = str(start + 8) + ':00:00'
+    address = address_generator()
+
+    branch = Branch.objects.create(
+        name_branch=name_branch,
+        opening_hours_start_branch=opening_hours_start,
+        opening_hours_end_branch=opening_hours_end,
+        address_branch=address
+    )
+
+    return branch
+
+
 if __name__ == '__main__':
+    '''
     for _ in range(50):
         client_generator()
+    '''
+    braches = list()
+    for _ in range(10):
+        braches.append(branch_generator())
