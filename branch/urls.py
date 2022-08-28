bdd223e974b6373338291ebeb79098a61ac86068
Matheus Simão Caixeta <101995486+MatheusSC017@users.noreply.github.com>
2022-08-28 09:39:07 -0300

    Criação de ViewSet para visualização dos endereços vinculados às filiais

--- branch/urls.py
+++ branch/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BranchViewSet
+from .views import BranchViewSet, BranchAddressViewSet
 
 router = DefaultRouter()
+router.register('enderecos', BranchAddressViewSet)
 router.register('', BranchViewSet)
 
 urlpatterns = [
--- branch/views.py
+++ branch/views.py
@@ -1,5 +1,7 @@
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
+from address.models import Address
+from address.serializers import AddressSerializer
 from .models import Branch
 from .serializers import BranchSerializer
 
@@ -8,3 +10,9 @@ class BranchViewSet(ModelViewSet):
     queryset = Branch.objects.all()
     serializer_class = BranchSerializer
     permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+
+
+class BranchAddressViewSet(ReadOnlyModelViewSet):
+    addresses = [b.address_branch.pk for b in Branch.objects.all()]
+    queryset = Address.objects.filter(pk__in=addresses)
+    serializer_class = AddressSerializer
