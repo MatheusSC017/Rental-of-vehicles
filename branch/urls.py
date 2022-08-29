ccce36aa18867be49a86daddb411be1d9469aaa2
Matheus Simão Caixeta <101995486+MatheusSC017@users.noreply.github.com>
2022-08-29 17:47:47 -0300

    Criação do endpoint para visualização dos veiculos em uma determinada filial

--- branch/urls.py
+++ branch/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import BranchViewSet, BranchAddressViewSet
+from .views import BranchViewSet, BranchAddressViewSet, BranchVehicleViewSet
 
 router = DefaultRouter()
 router.register('enderecos', BranchAddressViewSet)
+router.register('(?P<branch>[^/.]+)/veiculos', BranchVehicleViewSet)
 router.register('', BranchViewSet)
 
 urlpatterns = [
--- branch/views.py
+++ branch/views.py
@@ -2,6 +2,8 @@ from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 from rest_framework.permissions import DjangoModelPermissionsOrAnonReadOnly
 from address.models import Address
 from address.serializers import AddressSerializer
+from vehicle.models import Vehicle
+from vehicle.serializers import VehicleSerializer
 from .models import Branch
 from .serializers import BranchSerializer
 
@@ -16,3 +18,17 @@ class BranchAddressViewSet(ReadOnlyModelViewSet):
     addresses = [b.address_branch.pk for b in Branch.objects.all()]
     queryset = Address.objects.filter(pk__in=addresses)
     serializer_class = AddressSerializer
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+
+
+class BranchVehicleViewSet(ReadOnlyModelViewSet):
+    queryset = Vehicle.objects.all()
+    serializer_class = VehicleSerializer
+    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+
+    def get_queryset(self):
+        branch = self.kwargs.get('branch')
+
+        qs = Vehicle.objects.filter(branch_vehicle=branch)
+
+        return qs
