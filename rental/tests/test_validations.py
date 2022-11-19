f88267e81b6a6c3353c38f4880b6b54d3e253f12
Matheus Simão Caixeta <101995486+MatheusSC017@users.noreply.github.com>
2022-11-18 21:55:55 -0300

    Reorganização dos arquivos de testes do app de alugueis para facilitar manutenção e melhorias futuras

--- rental/tests/test_validations.py
+++ rental/tests/test_validations.py
@@ -1,83 +1,18 @@
 from rest_framework.test import APITestCase
 from datetime import date, timedelta
 from django.utils import timezone
-from django.urls import reverse
-from rest_framework import status
 from django.contrib.auth.models import User, Permission, ContentType
 from address.models import Address
 from client.models import Client
 from staff.models import StaffMember
 from branch.models import Branch
 from vehicle.models import Vehicle, VehicleClassification
-from .models import Rental, Insurance, AdditionalItems
-from . import validators
+from ..models import Rental, Insurance, AdditionalItems
+from .. import validators
 from random import choice, choices, randrange
 from validate_docbr import CPF, CNH, RENAVAM
 import faker
 import json
-
-
-class InsuranceTestCase(APITestCase):
-    @staticmethod
-    def json_generator():
-        fake = faker.Faker('pt_BR')
-        data = dict()
-        for _ in range(randrange(3, 8)):
-            data[fake.words(nb=1)[0]] = ' '.join(fake.words(nb=2))
-        return json.dumps(data)
-
-    def setUp(self) -> None:
-        self.fake = faker.Faker('pt_BR')
-
-        username = self.fake.first_name() + str(randrange(10000, 99999))
-        self.user = User.objects.create_user(
-            username=username,
-            email=username + '@email.com.br',
-            password=username
-        )
-
-        content_type = ContentType.objects.get_for_model(Insurance)
-        permissions = Permission.objects.filter(content_type=content_type)
-        for permission in permissions:
-            self.user.user_permissions.add(permission)
-
-        self.insurance = Insurance.objects.create(
-            title_insurance=' '.join(self.fake.words(nb=2)),
-            coverage_insurance=self.json_generator(),
-            price_insurance=randrange(100, 2000) / 100
-        )
-
-        self.list_url = reverse('Insurances-list')
-        self.detail_url = reverse('Insurances-detail', kwargs={'pk': self.insurance.pk})
-
-    def test_request_to_insurances_list(self):
-        response = self.client.get(self.list_url)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_request_to_insurance_creation(self):
-        self.client.force_login(self.user)
-        data = {
-            'title_insurance': ' '.join(self.fake.words(nb=2)),
-            'coverage_insurance': self.json_generator(),
-            'price_insurance': randrange(100, 2000) / 100
-        }
-        response = self.client.post(self.list_url, data=data)
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-    def test_request_to_insurance_update(self):
-        self.client.force_login(self.user)
-        data = {
-            'title_insurance': ' '.join(self.fake.words(nb=2)),
-            'coverage_insurance': self.json_generator(),
-            'price_insurance': randrange(100, 2000) / 100
-        }
-        response = self.client.put(self.detail_url, data=data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_request_to_insurance_delete(self):
-        self.client.force_login(self.user)
-        response = self.client.delete(self.detail_url)
-        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
 
 class ValidationsTestCase(APITestCase):
