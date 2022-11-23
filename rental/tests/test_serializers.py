b44b5cf9969967733bb8dedd2a067492af75acdd
Matheus Simão Caixeta <101995486+MatheusSC017@users.noreply.github.com>
2022-11-23 18:09:25 -0300

    Inclusão de teste para o serializador de itens adicionais

--- rental/tests/test_serializers.py
+++ rental/tests/test_serializers.py
@@ -17,8 +17,27 @@ class InsuranceSerializerTestCase(TestCase):
         data = self.serializer.data
         self.assertEqual(set(data.keys()), set(['id', 'title_insurance', 'coverage_insurance', 'price_insurance']))
 
-    def test_verify_serializer_fields_content(self) -> None:
+    def test_verify_contents_of_serializer_fields(self) -> None:
         data = self.serializer.data
         self.assertEqual(data['title_insurance'], self.insurance.title_insurance)
         self.assertEqual(data['coverage_insurance'], self.insurance.coverage_insurance)
         self.assertEqual(data['price_insurance'], self.insurance.price_insurance)
+
+
+class AdditionalItemsSerializerTestCase(TestCase):
+    def setUp(self) -> None:
+        self.additional_item = AdditionalItems.objects.create(
+            name_additionalitems='Additional Item Name',
+            daily_cost_additionalitems=2.5
+        )
+
+        self.serializer = AdditionalItemsSerializer(self.additional_item)
+
+    def test_verify_serializer_fields(self) -> None:
+        data = self.serializer.data
+        self.assertEqual(set(data.keys()), set(['id', 'name_additionalitems', 'daily_cost_additionalitems']))
+
+    def test_verify_contents_of_serializer_fields(self) -> None:
+        data = self.serializer.data
+        self.assertEqual(data['name_additionalitems'], self.additional_item.name_additionalitems)
+        self.assertEqual(data['daily_cost_additionalitems'], self.additional_item.daily_cost_additionalitems)
