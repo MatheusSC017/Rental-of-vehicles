b7a621990e01d1b3af46b902d829d98ec95042af
Matheus Simão Caixeta <101995486+MatheusSC017@users.noreply.github.com>
2022-11-27 09:07:05 -0300

    Inclusão de testes para a viewset de endereços

---
+++ address/tests/test_viewsets.py
@@ -0,0 +1,76 @@
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
+from django.contrib.auth.models import User, Permission, ContentType
+from ..models import Address
+import faker
+
+
+class AddressViewSetTestCase(APITestCase):
+    def setUp(self) -> None:
+        self.fake = faker.Faker('pt_BR')
+
+        username = self.fake.first_name() + '123456789'
+        self.user = User.objects.create_user(
+            username=username,
+            email=username + '@email.com',
+            password=username
+        )
+
+        content_type = ContentType.objects.get_for_model(Address)
+        permissions = Permission.objects.filter(content_type=content_type)
+        for permission in permissions:
+            self.user.user_permissions.add(permission)
+
+        self.address = Address.objects.create(
+            cep_address=self.fake.postcode(),
+            state_address=self.fake.estado_sigla(),
+            city_address=self.fake.city(),
+            district_address=self.fake.bairro(),
+            street_address=self.fake.street_name(),
+            number_address=self.fake.building_number()
+        )
+
+        self.list_url = reverse('Addresses-list')
+        self.detail_url = reverse('Addresses-detail', kwargs={'pk': self.address.pk})
+
+    def test_request_to_address_list(self) -> None:
+        self.client.force_login(self.user)
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_request_to_address_creation(self) -> None:
+        self.client.force_login(self.user)
+        data = {
+            'cep_address': self.fake.postcode(),
+            'state_address': self.fake.estado_sigla(),
+            'city_address': self.fake.city(),
+            'district_address': self.fake.bairro(),
+            'street_address': self.fake.street_name(),
+            'number_address': self.fake.building_number()
+        }
+        response = self.client.post(self.list_url, data=data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_request_to_address_detail(self) -> None:
+        self.client.force_login(self.user)
+        response = self.client.get(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_request_to_address_update(self) -> None:
+        self.client.force_login(self.user)
+        data = {
+            'cep_address': self.fake.postcode(),
+            'state_address': self.fake.estado_sigla(),
+            'city_address': self.fake.city(),
+            'district_address': self.fake.bairro(),
+            'street_address': self.fake.street_name(),
+            'number_address': self.fake.building_number()
+        }
+        response = self.client.put(self.detail_url, data=data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_request_to_address_delete(self) -> None:
+        self.client.force_login(self.user)
+        response = self.client.delete(self.detail_url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
