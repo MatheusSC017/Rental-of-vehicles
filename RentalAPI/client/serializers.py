d7e4256e413e13502ad00edf7bb57e5cd59bbd21
Matheus Simão Caixeta <101995486+MatheusSC017@users.noreply.github.com>
2024-01-23 18:44:22 -0300

    Updanting the serializer of clients and staff

--- RentalAPI/client/serializers.py
+++ RentalAPI/client/serializers.py
@@ -3,14 +3,16 @@ from django.contrib.auth.models import User
 from .models import Client
 
 
-class ClientSerializer(ModelSerializer):
-    class Meta:
-        model = Client
-        fields = '__all__'
-
-
 class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = ['pk', 'username', 'first_name', 'last_name', 'email', 'password', ]
         extra_kwargs = {'password': {'write_only': True}}
+
+
+class ClientSerializer(ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = Client
+        fields = '__all__'
