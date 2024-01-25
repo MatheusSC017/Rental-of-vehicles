2eaee285bc977bb38a880c1f7c966a9a6e5603ff
Matheus Simão Caixeta <101995486+MatheusSC017@users.noreply.github.com>
2024-01-25 07:23:37 -0300

    Excluding desnecessary code of the old endpoints

--- RentalAPI/rental/views.py
+++ RentalAPI/rental/views.py
@@ -41,44 +41,6 @@ class RentalViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, GenericVie
     permission_classes = [OnlyStaffMemberPermission, ]
     http_method_names = ['get', ]
 
-    def perform_create(self, serializer):
-        serializer.save(
-            staff=self.request.user.staffmember,
-            **self.fetch_values_to_fill_fields()
-        )
-
-    def perform_update(self, serializer):
-        serializer.save(
-            staff=self.request.user.staffmember,
-            **self.fetch_values_to_fill_fields()
-        )
-
-    def fetch_values_to_fill_fields(self) -> dict:
-        """ fetch the values for fields with autocomplete """
-        vehicle = get_object_or_404(Vehicle, renavam=self.request.data.get('vehicle'))
-        return {
-            'daily_cost': vehicle.classification.daily_cost,
-            'outlet_branch': vehicle.branch,
-            'additional_daily_cost': self.calculate_additional_daily_cost()
-        }
-
-    def calculate_additional_daily_cost(self) -> float:
-        """ Calculate de total additional daily cost
-        The total daily additional cost will be calculated based on the additional items received in the request,
-        returning the sum of the subtotals (cost of additional item times quantity) of each additional item
-        """
-        try:
-            # Get the list of additional items requested
-            relationship_additional_items_list = list(dict(self.request.data).get('additional_items'))
-            # The next step will be to create a list with the subtotal for each additional item
-            additional_items_list = (
-                AdditionalItems.objects.filter(pk=item['additional_item'])[0].daily_cost * item['number']
-                for item in relationship_additional_items_list
-            )
-            return sum(additional_items_list)
-        except TypeError:
-            return 0.
-
 
 class GenericRentalCreateViewSet(mixins.CreateModelMixin, mixins.UpdateModelMixin, GenericViewSet):
     queryset = Rental.objects.all()
