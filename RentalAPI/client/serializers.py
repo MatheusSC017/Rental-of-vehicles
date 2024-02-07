d4a82f5a719979934f17805743ed2e5cf850e06b
Matheus Simão Caixeta <101995486+MatheusSC017@users.noreply.github.com>
2024-02-07 17:50:58 -0300

    Updating client view and serializer
    Updating client view and serializer to include the user in the process of creation aupdate and delete

--- RentalAPI/client/serializers.py
+++ RentalAPI/client/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework.serializers import ModelSerializer
 from django.contrib.auth.models import User
+from django.db import transaction
 from .models import Client
 
 
@@ -11,14 +12,35 @@ class UserSerializer(ModelSerializer):
 
 
 class ClientSerializer(ModelSerializer):
-    class Meta:
-        model = Client
-        fields = ['cpf', 'rg', 'gender', 'age', 'phone', 'address', 'user', 'cnh', 'finance', ]
-
-
-class ClientReadOnlySerializer(ModelSerializer):
-    user = UserSerializer(read_only=True)
+    user = UserSerializer()
 
     class Meta:
         model = Client
         fields = ['cpf', 'rg', 'gender', 'age', 'phone', 'address', 'user', 'cnh', 'finance', ]
+
+    @transaction.atomic
+    def create(self, validated_data):
+        user_data = validated_data.pop('user')
+
+        user = User.objects.create_user(**user_data)
+
+        client = Client.objects.create(user=user, **validated_data)
+
+        return client
+
+    @transaction.atomic
+    def update(self, instance, validated_data):
+        user_instance = instance.user
+        user_data = validated_data.pop('user')
+
+        for attr, value in user_data.items():
+            setattr(user_instance, attr, value)
+
+        user_instance.save()
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+
+        return instance
--- RentalAPI/client/views.py
+++ RentalAPI/client/views.py
@@ -1,17 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
 from django.contrib.auth.models import User
 from .models import Client
-from .serializers import ClientSerializer, ClientReadOnlySerializer, UserSerializer
+from .serializers import ClientSerializer, UserSerializer
 
 
 class ClientViewSet(ModelViewSet):
     queryset = Client.objects.all()
     serializer_class = ClientSerializer
-
-    def get_serializer_class(self):
-        if self.action == 'list' or self.action == 'retrieve':
-            return ClientReadOnlySerializer
-        return ClientSerializer
 
 
 class UserViewSet(ModelViewSet):
