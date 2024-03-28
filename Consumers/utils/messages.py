29a9c721ed4110ecf6b32d8b4797bf088b5798d3
Matheus Simão Caixeta <101995486+MatheusSC017@users.noreply.github.com>
2024-03-28 07:35:04 -0300

    Replacing the PyWhatKit library with the 360 Dialog API to enable the use of more queues and improve performance

--- Consumers/utils/messages.py
+++ Consumers/utils/messages.py
@@ -1,6 +1,6 @@
-import pywhatkit as pwk
 import smtplib, ssl
 import pathlib
+import requests
 import os
 from jinja2 import Environment, FileSystemLoader
 from email.mime.text import MIMEText
@@ -12,6 +12,11 @@ try:
     load_dotenv(BASE_DIR.parent / ".env.consumers")
 except:
     load_dotenv(BASE_DIR.parent / ".env")
+
+if os.getenv("WHATSAPP_TEST_URL"):
+    WHATSAPP_URL = "https://waba-sandbox.360dialog.io/v1/messages"
+else:
+    WHATSAPP_URL = "https://waba.360dialog.io/v1/messages"
 
 
 def send_email(recipient, subject, **kwargs):
@@ -38,7 +43,25 @@ def send_whatsapp_message(recipient, subject, **kwargs):
     try:
         whatsapp_template = template_env.get_template(f"whatsapp/{subject.replace(' ', '_')}.html")
 
-        pwk.sendwhatmsg_instantly(recipient, whatsapp_template.render(**kwargs))
-        print("Message Sent!")
+        data = {
+            "messaging_product": "whatsapp",
+            "recipient_type": "individual",
+            "to": ''.join([c for c in recipient if c.isdigit()]),
+            "type": "text",
+            "text": {
+                "body": whatsapp_template.render(**kwargs)
+            }
+        }
+
+        headers = {
+            "D360-API-KEY": os.getenv("WHATSAPP_KEY")
+        }
+
+        response = requests.post(WHATSAPP_URL, json=data, headers=headers)
+
+        if response.status_code == 200:
+            print("Message sent successfully!")
+        else:
+            raise Exception(response)
     except Exception as e:
         print(f"Error in sending the message: {e}")
